99e3726aae12f1966e7242341c56b42503d97446
Cal Loomis <charles.loomis@relativity.com>
2021-09-26 05:20:55 -0500

    use main version from tag 0.35.0

--- internal/scaffold/main.go
+++ internal/scaffold/main.go
@@ -65,8 +65,8 @@ func runInteractive(params service.CollectorSettings) error {
 		return fmt.Errorf("failed to construct the collector server: %w", err)
 	}
 
-	cmd := service.NewCommand(app)
-	if err = cmd.Execute(); err != nil {
+	err = app.Run()
+	if err != nil {
 		return fmt.Errorf("collector server run finished with error: %w", err)
 	}
 
